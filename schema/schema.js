a3365a11c55b79288fd638a020b2da33d814d514
midimurphdesigns <midimurphdesigns@gmail.com>
2018-09-19 21:24:57 -0700

    updated schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,11 +1,11 @@
 const graphql = require("graphql");
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
-    id: { type: },
-    firstName: { type: },
-    age: { type: }
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
   }
 });
